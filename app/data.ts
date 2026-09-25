8c767301a8e080833b2e042649c39301a296f8db
Robin Chernak <robinchernak@gmail.com>
2026-09-24 18:51:10 -0700

    Add app/data.ts

---
+++ app/data.ts
@@ -0,0 +1,35 @@
+export const bookingUrl =
+  "https://online-booking.salonbiz.com/olb/web_booking.home?p_mobile=y&p_start=1&theme=1234&wbid=c1454t201505041904p3Ay8iJ843mrN7zrx6563";
+
+export const images = {
+  hero:
+    "https://static.wixstatic.com/media/714e96_316e957a523040399b8b7747403fc581~mv2.jpg",
+  salonWide:
+    "https://static.wixstatic.com/media/714e96_b61a6a478d264adb900df92de16eeec6~mv2.jpg",
+  salonWide2:
+    "https://static.wixstatic.com/media/714e96_e3f9be2655ab4937847aa369b5b27214~mv2.jpg",
+  retail:
+    "https://static.wixstatic.com/media/714e96_f151ca997ca747af9062f5c408426672~mv2_d_3000_2000_s_2.jpg",
+  storefront:
+    "https://static.wixstatic.com/media/714e96_1b0ea9831e064830b220d34e64300bb1~mv2.jpg",
+  franktown:
+    "https://static.wixstatic.com/media/714e96_2e48196c64804cc884725469a6ec1f88~mv2.jpg",
+  arches:
+    "https://static.wixstatic.com/media/714e96_b4552c154f844317b88ffb8edb1fe384~mv2.jpg"
+};
+
+export const services = [
+  ["Women’s cut", "$75"],
+  ["Men’s cut", "$45"],
+  ["Child cut", "$40"],
+  ["Root touch up", "$125"],
+  ["Full color", "$170"],
+  ["Partial highlight", "$180"],
+  ["Full highlight", "$250"],
+  ["Bleach & tone", "$195"],
+  ["Color correction", "$100/hr"],
+  ["Blow out", "$45"],
+  ["Blow out + thermal styling", "$55"],
+  ["Treatment add-on", "$35"],
+  ["Brazilian blowout", "$250"]
+];
